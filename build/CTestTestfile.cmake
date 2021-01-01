e518b27362f7893921e747e59c3c6e58b540ed21
nehamarne27 <nehamarne27gmail,com>
2021-01-01 19:36:29 +0530

    exctract feature using surf

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/neha/visual_odom/src
+# Build directory: /home/neha/visual_odom/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("visual_odom")
---
+++ src/visual_odom/src/capture_image.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import sys
+
+
+def main(args):
+    rospy.init_node('image_converter', anonymous=True)
+    print("i m in")
+    try:
+	cv_image = cv2.imread("/home/neha/Downloads/WhatsApp Image 2020-11-28 at 2.49.53 PM.jpeg",cv2.IMREAD_UNCHANGED)
+        sift = cv2.xfeatures2d.SIFT_create()
+        kp = sift.detect(cv_image, None)
+        cv_image = cv2.drawKeypoints(cv_image, kp, None)
+	cv2.imshow("neha", cv_image)
+	cv2.waitKey()
+	print("m out")
+    except KeyboardInterrupt:
+        print("Shutting Down")
+    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main(sys.argv)
