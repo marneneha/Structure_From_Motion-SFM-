1f1b9437c690c42f3a0b050ff6dc5d175d20982d
nehamarne27 <nehamarne27gmail,com>
2021-01-02 18:04:31 +0530

    able to detect surf feature

--- src/visual_odom/src/capture_image.py
+++ src/visual_odom/src/capture_image.py
@@ -16,10 +16,14 @@ def main(args):
 			temp = "/home/neha/Downloads/2011_09_26/2011_09_26_drive_0001_sync/image_00/data/000000000"+str(i)+".png"
 		else:
 			temp = "/home/neha/Downloads/2011_09_26/2011_09_26_drive_0001_sync/image_00/data/00000000"+str(i)+".png"
-		cv_image1 = cv2.imread(temp,cv2.IMREAD_GRAYSCALE)
-		cv2.imshow("img2", cv_image1)
+		
+		cv_image = cv2.imread(temp,cv2.IMREAD_GRAYSCALE)
+        	sift = cv2.xfeatures2d.SIFT_create()
+		kp1, ds1 = sift.detectAndCompute(cv_image, None)
+		cv_image = cv2.drawKeypoints(cv_image, kp1, None)
+		cv2.imshow("img2", cv_image)
 		print("m here")
-		cv2.waitKey(60)
+		cv2.waitKey(50)
 	print("m out")
     except KeyboardInterrupt:
         print("Shutting Down")
