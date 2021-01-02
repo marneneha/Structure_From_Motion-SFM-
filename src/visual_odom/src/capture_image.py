e9aa68342551421fa823ad8cea939c882b1fde55
nehamarne27 <nehamarne27gmail,com>
2021-01-02 17:57:18 +0530

    able to import vedio

--- src/visual_odom/src/capture_image.py
+++ src/visual_odom/src/capture_image.py
@@ -11,20 +11,15 @@ def main(args):
     rospy.init_node('image_converter', anonymous=True)
     print("i m in")
     try:
-	cv_image1 = cv2.imread("/home/neha/Downloads/neha.jpg",cv2.IMREAD_GRAYSCALE)
-	cv_image2 = cv2.imread("/home/neha/Downloads/neha (1).jpg",cv2.IMREAD_GRAYSCALE)
-        sift = cv2.xfeatures2d.SIFT_create()
-        kp1, ds1 = sift.detectAndCompute(cv_image1, None)
-        kp2, ds2 = sift.detectAndCompute(cv_image2, None)
-        cv_image1 = cv2.drawKeypoints(cv_image1, kp1, None)
-        cv_image2 = cv2.drawKeypoints(cv_image2, kp2, None)
-	cv2.imshow("img1", cv_image1)
-	cv2.imshow("img2", cv_image1)
-	bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
-	matches = bf.match(ds1, ds2)
-	matching_result = cv2.drawMatches(cv_image1,kp1,cv_image2,kp2,matches,None)
-	cv2.imshow("matching_result", matching_result)
-	cv2.waitKey()
+	for i in range(107):
+		if i<=9:
+			temp = "/home/neha/Downloads/2011_09_26/2011_09_26_drive_0001_sync/image_00/data/000000000"+str(i)+".png"
+		else:
+			temp = "/home/neha/Downloads/2011_09_26/2011_09_26_drive_0001_sync/image_00/data/00000000"+str(i)+".png"
+		cv_image1 = cv2.imread(temp,cv2.IMREAD_GRAYSCALE)
+		cv2.imshow("img2", cv_image1)
+		print("m here")
+		cv2.waitKey(60)
 	print("m out")
     except KeyboardInterrupt:
         print("Shutting Down")
