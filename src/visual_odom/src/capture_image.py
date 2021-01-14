bdc5e1fd34eb12e420583c8480a469d41a075760
nehamarne27 <nehamarne27gmail,com>
2021-01-14 12:14:19 +0530

    able to get x y of image

--- src/visual_odom/src/capture_image.py
+++ src/visual_odom/src/capture_image.py
@@ -5,26 +5,42 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import sys
-
+import numpy as np
 
 def main(args):
     rospy.init_node('image_converter', anonymous=True)
     print("i m in")
     try:
-	for i in range(107):
-		if i<=9:
-			temp = "/home/neha/Downloads/2011_09_26/2011_09_26_drive_0001_sync/image_00/data/000000000"+str(i)+".png"
-		else:
-			temp = "/home/neha/Downloads/2011_09_26/2011_09_26_drive_0001_sync/image_00/data/00000000"+str(i)+".png"
-		
-		cv_image = cv2.imread(temp,cv2.IMREAD_GRAYSCALE)
+		temp1 = "/home/neha/Downloads/2011_09_26/2011_09_26_drive_0001_sync/image_00/data/0000000001.png"
+		temp2 = "/home/neha/Downloads/2011_09_26/2011_09_26_drive_0001_sync/image_00/data/0000000002.png"
+		cv_image1 = cv2.imread(temp1,cv2.IMREAD_GRAYSCALE)
+		cv_image2 = cv2.imread(temp2,cv2.IMREAD_GRAYSCALE)
         	sift = cv2.xfeatures2d.SIFT_create()
-		kp1, ds1 = sift.detectAndCompute(cv_image, None)
-		cv_image = cv2.drawKeypoints(cv_image, kp1, None)
-		cv2.imshow("img2", cv_image)
-		print("m here")
-		cv2.waitKey(50)
-	print("m out")
+	        kp1, ds1 = sift.detectAndCompute(cv_image1, None)
+		kp2, ds2 = sift.detectAndCompute(cv_image2, None)
+		ds1=np.float32(ds1)
+    		ds2=np.float32(ds2)
+		cv_image1 = cv2.drawKeypoints(cv_image1, kp1, None)
+		cv_image2 = cv2.drawKeypoints(cv_image2, kp2, None)
+		cv2.imshow("img1", cv_image1)
+		cv2.imshow("img2", cv_image1)
+		bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=False)
+		matches = bf.match(ds1, ds2)
+		filtered_matches = []
+		for m in matches:
+			#print(m.distance)
+			if m.distance<500:
+				filtered_matches.append(m)
+		kep1=np.float32([kp1[m.queryIdx].pt for m in filtered_matches])
+		kep2=np.float32([kp2[m.trainIdx].pt for m in filtered_matches])
+		matching_result = cv2.drawMatches(cv_image1,kp1,cv_image2,kp2,matches,None)
+		cv2.imshow("matching_result", matching_result)
+		print(kep1)
+		print(kep2)
+		print(len(kep1))
+		print(len(kep2))
+		cv2.waitKey()	
+		print("m out")
     except KeyboardInterrupt:
         print("Shutting Down")
     cv2.destroyAllWindows()
