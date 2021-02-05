8d0e37d5ec7c691a4c680da57fc312c34b7bfa27
nehamarne27 <nehamarne27gmail,com>
2021-02-05 07:22:41 +0530

    this code is with cv2.traingulate() we will move without it

--- src/visual_odom/src/capture_image.py
+++ src/visual_odom/src/capture_image.py
@@ -11,10 +11,13 @@ def main(args):
     rospy.init_node('image_converter', anonymous=True)
     print("i m in")
 #K_00: 9.842439e+02 0.000000e+00 6.900000e+02 0.000000e+00 9.808141e+02 2.331966e+02 0.000000e+00 0.000000e+00 1.000000e+00
+# this is calibration matrix diagonal values are focal length w.r.t. x y z axis   
     k =np.array([[9.842439e+02, 0.000000e+00, 6.900000e+02], 
              [0.000000e+00, 9.808141e+02, 2.331966e+02], 
              [0.000000e+00, 0.000000e+00, 1.000000e+00]])
-    proj = np.array([[7.215377e+02,0.000000e+00,6.095593e+02, 0.000000e+00],[0.000000e+00, 7.215377e+02, 1.728540e+02, 0.000000e+00],[0.000000e+00, 0.000000e+00, 1.000000e+00, 0.000000e+00,]])
+    proj = np.array([[7.215377e+02,0.000000e+00,6.095593e+02, 0.000000e+00],
+		     [0.000000e+00, 7.215377e+02, 1.728540e+02, 0.000000e+00],
+		     [0.000000e+00, 0.000000e+00, 1.000000e+00, 0.000000e+00,]])
     temp1 = "/home/neha/Downloads/2011_09_26/2011_09_26_drive_0001_sync/image_00/data/0000000000.png"
     cv_image1 = cv2.imread(temp1,cv2.IMREAD_GRAYSCALE)
     try:
@@ -37,16 +40,18 @@ def main(args):
 		cv2.imshow("img2", cv_image2)
 		bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 		matches = bf.match(ds1, ds2)
+		# queryIdx corresponds to index for 1st img trainIdx corresponds from 2nd img
 		kep1=np.float32([kp1[m.queryIdx].pt for m in matches])
 		kep2=np.float32([kp2[m.trainIdx].pt for m in matches])
 		matching_result = cv2.drawMatches(cv_image1,kp1,cv_image2,kp2,matches,None)
 		#cv2.imshow("matching_result", matching_result)
+		#finds Essential Matrix 
             	E,mask=cv2.findEssentialMat(kep2,kep1,k,cv2.RANSAC,prob=0.999,threshold=0.1, mask=None)
 		retval,rmat,trans,mask=cv2.recoverPose(E,kep1,kep2,k)
+#this is ore like correction 
+		new_cloud=triangulation(rmat,trans,kep1,kep2,k)
+		new_cloud = new_cloud/new_cloud[3]
 		cv_image1 = cv_image2
-		for i in range(kep1):
-			3d_pt = cv2.triangulatePoints(kep1,proj)
-		print(3d_pt)
 		print("m here")
 		print(kep1)
 		print(kep2)
@@ -55,10 +60,27 @@ def main(args):
 		print(E)
 		print(rmat)
 		print(trans)
+		print(new_cloud)
+		print(len(new_cloud))
 		print("m out")
 		cv2.waitKey(10)	
     except KeyboardInterrupt:
         print("Shutting Down")
     cv2.destroyAllWindows()
+
+def triangulation(R, t, kp0, kp1, K):
+    P0 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
+    P0 = K.dot(P0)
+    P1 = np.hstack((R, t))
+    P1 = K.dot(P1)
+    kp0_3d=np.ones((3,kp0.shape[0]))
+    kp1_3d=np.ones((3,kp1.shape[0]))
+    kp0_3d[0], kp0_3d[1] = kp0[:, 0].copy(), kp0[:, 1].copy()
+    kp1_3d[0], kp1_3d[1] = kp1[:, 0].copy(), kp1[:, 1].copy()
+    cloud = cv2.triangulatePoints(P0, P1, kp0_3d[:2],kp1_3d[:2])
+#     cloud/=cloud[3]
+    cloud=cloud.T
+    return cloud[:,:3]
+
 if __name__ == '__main__':
     main(sys.argv)
